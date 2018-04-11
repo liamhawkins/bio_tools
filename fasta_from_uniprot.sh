b1962ab2859072c1fcedfafbb66f054e65738cd1
Liam Hawkins <liam.hawkins@carleton.ca>
2018-04-11 16:50:57 -0400

    Add usage comments

--- fasta_from_uniprot.sh
+++ fasta_from_uniprot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Takes list of UNIPROT accession IDs and downloads sequences in FASTA format from UNIPROT
+# Usage: ./fasta_from_uniprot.sh <ACCESSION_LIST.txt>
 
 sort $1 | uniq | cut -d ';' -f 1 | tr -d '\r' > uniq_ids.temp
 mkdir -p ./uniprot_fastas
--- multiline_to_oneline_fasta.sh
+++ multiline_to_oneline_fasta.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Converts multiline FASTA (FASTA with line breaks in sequences) to FASTA with oneline sequences
+# Usage: ./multiline_to_oneline_fasta.sh <INPUT.FASTA> > <OUTPUT.FASTA>
 
 awk '/^>/ { print (NR==1 ? "" : RS) $0; next } { printf "%s", $0 } END { printf RS }' $1
 
